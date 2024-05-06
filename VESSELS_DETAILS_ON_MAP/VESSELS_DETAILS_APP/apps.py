0d67047fdda7b19c6d4ac3ab6c0d2c625b7f0068
Subrat Routray <raysubrat01@gmail.com>
2024-05-06 16:08:22 +0530

    Delete VESSELS_DETAILS_ON_MAP directory

--- VESSELS_DETAILS_ON_MAP/VESSELS_DETAILS_APP/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VesselsDetailsAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'VESSELS_DETAILS_APP'
--- VESSELS_DETAILS_ON_MAP/VESSELS_DETAILS_APP/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-from .models import RouteDetails
-
-class VesselNumberForm(forms.Form):
-    vessel_number = forms.ModelChoiceField(queryset=RouteDetails.objects.all().values_list('vessel_number', flat=True))
